6a6b447637add1c1affdfadc6000e1722a57c2df
Iyad Allouch <eyad.2002.alloch@hotmail.com>
2024-07-12 00:20:06 +0400

    Create SignUp page

--- client/src/Pages/UserChoose.js
+++ client/src/Pages/UserChoose.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "../style/UserChooseStyle.css";
-import backgroundImage from '../images/logo.png'; 
+import WebsiteLogo from "../images/logo.png";
+import { Link } from "react-router-dom";
 
 export default function UserChoose() {
   return (
     <div className="background-page">
-    <img src={backgroundImage} alt="Background" className="background-image" />
-    <h1>Login as</h1>
-    <div className="button-container">
-      <button className="action-button">User</button>
-      <button className="action-button">Programmer </button>
-      <h2>Or</h2>
-      <button className="action-button">Create new account</button>
+      <img
+        src={WebsiteLogo}
+        alt="Background"
+        className="background-image"
+      />
+      <h1>Login as</h1>
+      <div className="button-container">
+        <button className="action-button">User</button>
+        <button className="action-button">Programmer </button>
+        <h2>Or</h2>
+        <Link to="/signup" aria-current="page">
+          <button className="action-button">Create new account</button>
+        </Link>
+      </div>
     </div>
-  </div>
   );
 }
